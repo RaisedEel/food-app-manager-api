febcd5f1be9583068443db78c6790a50efa2f630
Antonio Aguilera <antonhy.romero98@gmail.com>
2023-05-01 22:43:02 -0600

    Commit inicial.

---
+++ src/main/java/com/raisedeel/foodappmanager/FoodAppManagerApplication.java
@@ -0,0 +1,13 @@
+package com.raisedeel.foodappmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodAppManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodAppManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/raisedeel/foodappmanager/FoodAppManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raisedeel.foodappmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodAppManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
