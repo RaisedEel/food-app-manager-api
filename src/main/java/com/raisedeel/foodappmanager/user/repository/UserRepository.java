d5302a266542876751299a8b6faebc8bd67798f6
Antonio Aguilera <antonhy.romero98@gmail.com>
2023-05-03 18:21:40 -0600

    Agregada dependencia mapstruct para facilitar el uso de dto. Agregada función para buscar usuario por email.

--- src/main/java/com/raisedeel/foodappmanager/user/repository/UserRepository.java
+++ src/main/java/com/raisedeel/foodappmanager/user/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.raisedeel.foodappmanager.user.repository;
 import com.raisedeel.foodappmanager.user.model.User;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends CrudRepository<User, Long> {
+  Optional<User> findByEmail(String email);
 }
